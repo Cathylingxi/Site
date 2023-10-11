d31865e7e8e0a8531903eddd9c41b1c3e60907c6
LingxiYang <23970574@qq.com>
2023-10-11 11:31:54 +0800

    后端项目创建
    初始化后端项目

---
+++ site_go/src/main/java/com/edu/cn/site_go/SiteGoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.cn.site_go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteGoApplication.class, args);
+    }
+
+}
---
+++ site_go/src/test/java/com/edu/cn/site_go/SiteGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.cn.site_go;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
